2f49aeadf75976de29241998a9cd97c6f2991ef1
Kwon Gihun <148840826+GRetriever@users.noreply.github.com>
2023-11-23 09:08:33 +0900

    Time: 53 ms (39.25%), Space: 16.3 MB (9.49%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        answer = ''
+        for i in s:
+            if i == 'i':
+                answer = answer[::-1]
+            else:
+                answer += i
+        return answer
