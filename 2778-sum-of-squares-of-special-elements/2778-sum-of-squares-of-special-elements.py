2b486c1c2cc0992724c2a689f328b0f35d340c0e
Kwon Gihun <148840826+GRetriever@users.noreply.github.com>
2023-11-28 09:18:32 +0900

    Time: 71 ms (31.64%), Space: 16.3 MB (36.32%) - LeetHub

---
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        # answer = 0
+        # n = len(nums)
+        # for i in range(1,n+1):
+        #     if n % i == 0:
+        #         answer += nums[i-1]**2
+        # return answer
+        answer = 0
+        n = len(nums)
+        for i,num in enumerate(nums):
+            if n % (i+1) == 0:
+                answer += nums[i]**2
+        return answer
