91a32b320cba68a1a46c4a809da98d7f9b4f285f
Kwon Gihun <xzv2221@gmail.com>
2023-12-29 09:08:16 +0900

    Time: 360 ms (8.23%), Space: 17.4 MB (11.05%) - LeetHub

--- 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.py
+++ 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.py
@@ -1,7 +1,7 @@
 class Solution:
     def sumCounts(self, nums: List[int]) -> int:
-        answer = []
+        answer = 0
         for i in range(len(nums)):
-            for j in range(i,len(nums)+1):
-                answer.append(len(set(nums[i:j]))**2)
-        return sum(answer)
+            for j in range(i+1,len(nums)+1):
+                answer += (len(set(nums[i:j]))**2)
+        return answer
