bc31879aa243cb3f2dd74a7d419785be0c85eb53
Kwon Gihun <xzv2221@gmail.com>
2024-01-11 09:05:39 +0900

    Time: 38 ms (55.84%), Space: 17.4 MB (9.46%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.py
@@ -1,10 +1,10 @@
 class Solution:
     def squareIsWhite(self, coordinates: str) -> bool:
-        if coordinates[0] in ('a','c','e','g') and int(coordinates[1]) % 2 == 1:
+        if coordinates[0] in ('a','c','e','g') and int(coordinates[1]) in (1,3,5,7):
             return False
-        elif coordinates[0] in ('a','c','e','g') and int(coordinates[1]) % 2 == 0:
+        elif coordinates[0] in ('a','c','e','g') and int(coordinates[1]) in (2,4,6,8):
             return True
-        elif coordinates[0] in ('b','d','f','h') and int(coordinates[1]) % 2 == 1:
+        elif coordinates[0] in ('b','d','f','h') and int(coordinates[1]) in (1,3,5,7):
             return True
-        elif coordinates[0] in ('b','d','f','h') and int(coordinates[1]) % 2 == 0:
+        elif coordinates[0] in ('b','d','f','h') and int(coordinates[1]) in (2,4,6,8):
             return False
