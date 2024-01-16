eb9e1f3f0e4f849c7b0505ae2232633bbffd29b4
Kwon Gihun <xzv2221@gmail.com>
2024-01-16 09:42:32 +0900

    Time: 92 ms (92.50%), Space: 17.6 MB (34.36%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,15 +1,24 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        primary = []
-        secondary = []
-        for i in range(len(mat)):
-            primary.append(mat[i][i])
-        x = len(mat) -1
-        y = 0
-        for j in range(len(mat)):
-            secondary.append(mat[y][x])
-            x -=1
-            y +=1
-        if len(primary) % 2 == 1:
-            return sum(primary) + sum(secondary) - (primary[len(primary)//2])
-        return sum(primary) + sum(secondary)
+        # primary = []
+        # secondary = []
+        # for i in range(len(mat)):
+        #     primary.append(mat[i][i])
+        # x = len(mat) -1
+        # y = 0
+        # for j in range(len(mat)):
+        #     secondary.append(mat[y][x])
+        #     x -=1
+        #     y +=1
+        # if len(primary) % 2 == 1:
+        #     return sum(primary) + sum(secondary) - (primary[len(primary)//2])
+        # return sum(primary) + sum(secondary)
+
+        answer = 0
+        l = len(mat)
+        for i in range(l):
+            answer += mat[i][i]
+            answer += mat[l-1-i][i]
+        if len(mat) % 2 == 1:
+            answer -= mat[l//2][l//2]
+        return answer
