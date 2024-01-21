3daba1039c5922d7e347113cefdd991c72a38496
Kwon Gihun <xzv2221@gmail.com>
2024-01-22 08:55:11 +0900

    Time: 61 ms (79.92%), Space: 16.8 MB (33.63%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
@@ -1,12 +1,18 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        answer = []
-        total = nums1+nums2
-        for i in range(len(total)):
-            for j in range(i+1,len(total)):
-                if total[i][0] == total[j][0]:
-                    answer.append([total[i][0],total[i][1]+total[j][1]])
-                    nums1.remove(total[i])
-                    nums2.remove(total[j])
-        return sorted(answer+nums1+nums2)
-                
+        # answer = []
+        # total = nums1+nums2
+        # for i in range(len(total)):
+        #     for j in range(i+1,len(total)):
+        #         if total[i][0] == total[j][0]:
+        #             answer.append([total[i][0],total[i][1]+total[j][1]])
+        #             nums1.remove(total[i])
+        #             nums2.remove(total[j])
+        # return sorted(answer+nums1+nums2)
+        dict = defaultdict(int)
+        for id,val in nums1 + nums2:
+            if id in dict:
+                dict[id] += val
+            else:
+                dict[id] = val
+        return sorted(list(dict.items()))
