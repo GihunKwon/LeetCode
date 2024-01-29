d96291e7e382590dfc9a1dd84963f7d5e60a31ae
Kwon Gihun <xzv2221@gmail.com>
2024-01-30 08:17:47 +0900

    Time: 1269 ms (70.74%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 SELECT A.product_name,SUM(B.unit) AS unit
 FROM Products A
-LEFT JOIN Orders B ON A.product_id = B.product_id
+RIGHT JOIN Orders B ON A.product_id = B.product_id
 WHERE B.order_date LIKE '2020-02%'
-GROUP BY 1
-HAVING unit >= 100
+GROUP BY A.product_id
+HAVING SUM(B.unit) >= 100
