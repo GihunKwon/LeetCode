2cf5cf5ebabdaa0c229897e4e13fe3b1dd5aa4b9
Kwon Gihun <xzv2221@gmail.com>
2024-01-31 09:17:53 +0900

    Time: 34 ms (77.95%), Space: 16.6 MB (46.12%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.py
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.py
@@ -1,16 +1,15 @@
 class Solution:
     def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
-        alp = 'abcdefghij'
         first = ''
         second = ''
         target = ''
         for i in firstWord:
-            first += str(alp.index(i))
+            first += str(ord(i) - 97)
         for j in secondWord:
-            second += str(alp.index(j))
+            second += str(ord(j) - 97)
         for k in targetWord:
-            target += str(alp.index(k))
-        if int(first) + int(second) == int(target):
-            return True
-        else:
-            return False
+            target += str(ord(k) - 97)
+        
+        print(int(first),int(second),int(target))
+        print(first,second,target)
+        return int(first) + int(second) == int(target)
