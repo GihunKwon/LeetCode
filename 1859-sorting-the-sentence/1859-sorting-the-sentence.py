a2fe856602c0d1aba7678674a8cf629cd5166ccb
Kwon Gihun <xzv2221@gmail.com>
2024-01-31 09:05:08 +0900

    Time: 37 ms (59.01%), Space: 16.4 MB (70.11%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,9 +1,8 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        sent = {}
-        answer = []
-        for i in s.split():
-            sent[int(i[-1])] = i[:-1]
-        for j in range(1,len(s.split())+1):
-            answer.append(sent[j])
-        return ' '.join(answer)
+        s = s.split(' ')
+        answer = [0] * len(s)
+        for i in s:
+            answer[int(i[-1])-1] = i[:-1]
+        return ' '.join(answer)    
+                
