d39ca8933fee0d46729ed3487ce4d040bb016de6
Kwon Gihun <xzv2221@gmail.com>
2024-02-06 09:06:05 +0900

    Time: 50 ms (10.19%), Space: 16.5 MB (61.02%) - LeetHub

--- 3028-ant-on-the-boundary/3028-ant-on-the-boundary.py
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.py
@@ -1,9 +1,9 @@
 class Solution:
     def returnToBoundaryCount(self, nums: List[int]) -> int:
         answer = 0
-        boundary = 0
-        for num in nums:
-            boundary += num
-            if boundary == 0:
+        cur = 0
+        for i in nums:
+            cur += i
+            if cur == 0:
                 answer += 1
         return answer
