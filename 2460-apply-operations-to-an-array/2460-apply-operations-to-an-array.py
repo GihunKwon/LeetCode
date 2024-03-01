7b04d978632d248afd42344f4357b2d1fc686554
Kwon Gihun <xzv2221@gmail.com>
2024-03-01 09:47:42 +0900

    Time: 55 ms (33.07%), Space: 16.9 MB (70.19%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+        cnt = nums.count(0)
+        nums = [num for num in nums if num != 0]
+        return nums +  ([0] * cnt)
