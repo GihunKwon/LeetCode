5ce163edac5b3db91690b05c0f66ef6fa2852566
Kwon Gihun <xzv2221@gmail.com>
2024-03-05 08:16:09 +0900

    Time: 61 ms (84.88%), Space: 16.5 MB (71.35%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.py
+++ 1399-count-largest-group/1399-count-largest-group.py
@@ -1,15 +1,20 @@
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        answer = {}
-        
-        for i in range(1,n+1):
-            i = sum(map(int,str(i)))
-            
-            if i not in answer:
-                answer[i] = 1
-            else:
-                answer[i] += 1
+        # answer = {}
+        # for i in range(1,n+1):
+        #     i = sum(map(int,str(i)))
+        #     if i not in answer:
+        #         answer[i] = 1
+        #     else:
+        #         answer[i] += 1
+        # m = max(answer.values())
+        # return sum(1 for i in answer.values() if i == m)
 
-        m = max(answer.values())
-        
-        return sum(1 for i in answer.values() if i == m)
+        cnt = Counter()
+        for i in range(1,n+1):
+            total = 0
+            while i:
+                total += i%10
+                i = i//10
+            cnt[total] += 1
+        return list(cnt.values()).count(max(cnt.values()))
