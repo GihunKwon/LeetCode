fe481e9dee6d732cf84219e41533d399a3aba10e
Kwon Gihun <xzv2221@gmail.com>
2024-03-12 08:24:51 +0900

    Time: 647 ms (62.26%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+SELECT visited_on,
+    (select SUM(amount)
+    from Customer
+    where visited_on BETWEEN DATE_SUB(A.visited_on,INTERVAL 6 DAY) AND A.visited_on
+    ) AS amount,
+    ROUND(
+        (select SUM(amount)/7
+        from Customer
+        WHERE visited_on BETWEEN DATE_SUB(A.visited_on,INTERVAL 6 DAY) AND A.visited_on
+    ),2) AS average_amount
+FROM Customer A
+WHERE visited_on >= (
+    select DATE_ADD(MIN(visited_on),INTERVAL 6 DAY)
+    from Customer
+)
+GROUP BY visited_on
