08a45a6045a14d55ebdbe1a0d7bbc355885b74eb
Kwon Gihun <xzv2221@gmail.com>
2024-03-17 09:47:55 +0900

    Time: 1340 ms (83.38%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 SELECT person_name
 FROM (
-    select person_name,turn,sum(weight) over (order by turn) as cum
+    select person_name,turn,sum(weight) over (order by turn) as cum 
     from Queue) A
 WHERE cum <= 1000
 ORDER BY turn DESC
-LIMIT 1 
+LIMIT 1
