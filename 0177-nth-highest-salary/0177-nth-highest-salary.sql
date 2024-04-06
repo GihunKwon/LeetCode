5dfc3c05332c0d2380d0446c83ead3fe87264c43
Kwon Gihun <xzv2221@gmail.com>
2024-04-06 10:14:52 +0900

    Time: 1546 ms (5.05%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,11 +1,10 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 SET N = N-1;
-  RETURN (
-      select DISTINCT salary
+    RETURN (
+        select DISTINCT salary
         from Employee
         order by salary DESC
         limit 1 OFFSET N
-
- );
+    );
 END
