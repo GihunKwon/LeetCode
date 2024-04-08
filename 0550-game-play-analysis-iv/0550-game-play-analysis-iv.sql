90d724ac69431c276f42b8fb03b0474f435377cb
Kwon Gihun <xzv2221@gmail.com>
2024-04-08 09:41:55 +0900

    Time: 874 ms (90.69%), Space: 0B (100.00%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,13 +1,8 @@
 # Write your MySQL query statement below
-SELECT ROUND(
-    COUNT(DISTINCT player_id)
-    /
-    (select COUNT(DISTINCT player_id) from Activity)
-    , 2 ) AS fraction
+SELECT ROUND(COUNT(DISTINCT player_id) / (select COUNT(DISTINCT player_id) from Activity),2) AS fraction
 FROM Activity
-WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) 
-    IN (
-        select player_id, MIN(event_date) AS first_login
-        from Activity
-        GROUP BY player_id
-    )
+WHERE (player_id,DATE_SUB(event_date, INTERVAL 1 DAY)) IN (
+    select player_id,min(event_date)
+    from Activity
+    group by player_id
+)
