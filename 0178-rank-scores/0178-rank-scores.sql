61268010a9bccfa689037955aba9387dc7a9db2f
Kwon Gihun <xzv2221@gmail.com>
2024-04-12 10:32:31 +0900

    Time: 641 ms (38.80%), Space: 0B (100.00%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+SELECT score, DENSE_RANK() OVER (order by score desc) as 'rank'
 FROM Scores
