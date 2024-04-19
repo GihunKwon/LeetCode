83248feecae95ae1ebad6739e0fdc182996d2680
Kwon Gihun <xzv2221@gmail.com>
2024-04-19 10:16:55 +0900

    Time: 1790 ms (18.63%), Space: 18.4 MB (97.42%) - LeetHub

---
+++ 0888-fair-candy-swap/0888-fair-candy-swap.py
@@ -0,0 +1,6 @@
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        diff = (sum(aliceSizes) - sum(bobSizes)) // 2
+        for i in bobSizes:
+            if i + diff in aliceSizes:
+                return [i+diff,i]
