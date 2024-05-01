8fdc83eb259442bcac99f52ddc792ff6b0136a8d
Kwon Gihun <xzv2221@gmail.com>
2024-05-02 08:59:59 +0900

    Time: 483 ms (99.97%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -4,9 +4,9 @@ SELECT visited_on,
     from Customer
     where visited_on BETWEEN DATE_SUB(A.visited_on,INTERVAL 6 DAY) AND A.visited_on) AS amount,
     ROUND(
-        (select SUM(amount)/7
+        (select SUM(amount) / 7
         from Customer
-        where visited_on BETWEEN DATE_SUB(A.visited_on,INTERVAL 6 DAY) AND A.visited_on),2) AS average_amount
+        where visited_on BETWEEN DATE_SUB(A.visited_on,INTERVAL 6 DAY) AND A.visited_on),2) AS average_amount 
 FROM Customer A
 WHERE visited_on >= (
     select DATE_ADD(MIN(visited_on),INTERVAL 6 DAY)
