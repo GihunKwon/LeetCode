2297bb395ed84e41efe75e717f34c6c76bedac50
Kwon Gihun <xzv2221@gmail.com>
2024-05-03 08:11:38 +0900

    Time: 1923 ms (5.03%), Space: 0B (100.00%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
 SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016
 FROM Insurance
-WHERE tiv_2015 IN (
-    select tiv_2015
-    from Insurance
-    group by tiv_2015
-    having COUNT(*) > 1
-)
-AND (lat,lon) IN (
+WHERE (lat,lon) NOT IN (
     select lat,lon
     from Insurance
     group by lat,lon
-    having COUNT(*) = 1
+    having COUNT(lat) > 1 AND COUNT(lon) > 1
+)
+AND (tiv_2015) IN (
+    select tiv_2015
+    from Insurance
+    group by tiv_2015
+    having COUNT(tiv_2015) > 1
 )
