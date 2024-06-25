7c4903b51c01233d2faa9e5988ba249651740cd4
Gihun Kwon <xzv2221@gmail.com>
2024-06-25 09:13:55 -0700

    Time: 1049 ms (92.22%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,17 +1,16 @@
 # Write your MySQL query statement below
-(select B.name AS results
-from MovieRating C
-join Users B ON C.user_id = B.user_id
-group by B.name
-order by COUNT(*) DESC,B.name
-limit 1)
-
+(SELECT B.name AS results
+FROM MovieRating C
+JOIN Users B ON C.user_id = B.user_id
+GROUP BY B.name
+ORDER BY COUNT(*) DESC, B.name
+LIMIT 1)
 UNION ALL
-
-(select A.title AS results
-from MovieRating C
-join Movies A ON C.movie_id = A.movie_id
-where C.created_at LIKE '2020-02%'
-group by A.title
-order by AVG(rating) DESC,A.title
-limit 1)
+(SELECT A.title AS results
+FROM MovieRating C
+JOIN Movies A ON C.movie_id = A.movie_id
+WHERE YEAR(C.created_at) = '2020'
+        AND MONTH(C.created_at) = '02'
+GROUP BY C.movie_id
+ORDER BY AVG(C.rating) DESC, A.title
+LIMIT 1)
